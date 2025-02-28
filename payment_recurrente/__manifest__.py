d3e68c2370403032fcda04a3bca11c943e2b7466
zamsoss <zamsoss@MacBook-Air-de-zamsoss.local>
2025-02-28 17:42:40 -0600

    Update Version

--- payment_recurrente/__manifest__.py
+++ payment_recurrente/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Payment Provider: Recurrente',
-    'version': '1.0',
+    'version': '1.1',
     'category': 'Accounting/Payment Providers',
     'summary': "Integration with Recurrente payment gateway.",
     'depends': ['payment'],
